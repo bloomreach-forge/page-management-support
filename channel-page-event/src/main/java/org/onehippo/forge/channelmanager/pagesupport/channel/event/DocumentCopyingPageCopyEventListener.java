4b94bf332a0363fddce586d56da6838e2003611b
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2015-12-16 20:41:15 +0000

    Inject a Predicate class that makes sure that inherited component configurations which *do not* get copied (because the target already has them) also do not get their documents copied by skipping them from the DocumentParamsScanner find document paths recursive

--- channel-page-event/src/main/java/org/onehippo/forge/channelmanager/pagesupport/channel/event/DocumentCopyingPageCopyEventListener.java
+++ channel-page-event/src/main/java/org/onehippo/forge/channelmanager/pagesupport/channel/event/DocumentCopyingPageCopyEventListener.java
@@ -16,18 +16,26 @@
 package org.onehippo.forge.channelmanager.pagesupport.channel.event;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
 
 import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.configuration.components.HstComponentConfiguration;
 import org.hippoecm.hst.configuration.hosting.Mount;
+import org.hippoecm.hst.configuration.site.HstSite;
 import org.hippoecm.hst.core.container.ComponentManager;
 import org.hippoecm.hst.core.container.ComponentManagerAware;
+import org.hippoecm.hst.core.jcr.RuntimeRepositoryException;
 import org.hippoecm.hst.core.linking.DocumentParamsScanner;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.hippoecm.hst.pagecomposer.jaxrs.api.PageCopyContext;
@@ -39,8 +47,7 @@ import org.hippoecm.repository.util.JcrUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.eventbus.AllowConcurrentEvents;
-import com.google.common.eventbus.Subscribe;
+import static org.hippoecm.hst.configuration.HstNodeTypes.COMPONENT_PROPERTY_REFERECENCECOMPONENT;
 
 /**
  * <code>org.hippoecm.hst.pagecomposer.jaxrs.api.PageCopyEvent</code> event handler which is to be registered through
@@ -141,7 +148,7 @@ public class DocumentCopyingPageCopyEventListener implements ComponentManagerAwa
                                             + sourceContentBasePath + "'. Target='" + targetContentBasePath + "'.");
                         }
 
-                        final Set<String> documentPathSet = getDocumentPathSetInPage(pageCopyContext.getSourcePage());
+                        final Set<String> documentPathSet = getDocumentPathSetInPage(pageCopyContext);
 
                         if (!documentPathSet.isEmpty()) {
                             if (!StringUtils.equals(sourceMount.getContentPath(), targetMount.getContentPath())) {
@@ -264,13 +271,76 @@ public class DocumentCopyingPageCopyEventListener implements ComponentManagerAwa
      * with a '/' or without. If they start with a '/', they are absolute
      * paths (from jcr root). If they don't start with a '/', they are
      * relative to the channel content root.
-     * @param pageConfig
+     * @param pageCopyContext
      * @return
      */
-    private Set<String> getDocumentPathSetInPage(final HstComponentConfiguration pageConfig) {
-        List<String> documentPathList = DocumentParamsScanner.findDocumentPathsRecursive(pageConfig,
-                Thread.currentThread().getContextClassLoader());
+    private Set<String> getDocumentPathSetInPage(final PageCopyContext pageCopyContext) throws RepositoryException {
+        final FilterPresentComponentConfigurations filterPresentComponentConfigurations
+                = new FilterPresentComponentConfigurations(pageCopyContext.getSourcePage(), pageCopyContext.getEditingMount().getHstSite(),
+                pageCopyContext.getTargetMount().getHstSite(),
+                pageCopyContext.getRequestContext().getSession());
+
+        List<String> documentPathList = DocumentParamsScanner.findDocumentPathsRecursive(pageCopyContext.getSourcePage(),
+                Thread.currentThread().getContextClassLoader(), filterPresentComponentConfigurations);
+
         return new LinkedHashSet<String>(documentPathList);
+    }
+
+    public static class FilterPresentComponentConfigurations implements Predicate<HstComponentConfiguration> {
+
+        private final Set<String> filteredConfigurationUUIDs;
+        public FilterPresentComponentConfigurations(final HstComponentConfiguration sourceConfig, final HstSite sourceSite, final HstSite targetSite, final Session session) {
+            filteredConfigurationUUIDs =  new HashSet<>();
+            populateSkipSet(sourceConfig, sourceSite, targetSite, session, filteredConfigurationUUIDs);
+        }
+
+        private void populateSkipSet(final HstComponentConfiguration sourceConfig,
+                                     final HstSite sourceSite,
+                                     final HstSite targetSite,
+                                     final Session session,
+                                     final Set<String> skipSet) {
+            try {
+                final Node sourceNode = session.getNodeByIdentifier(sourceConfig.getCanonicalIdentifier());
+                if (sourceNode.hasProperty(COMPONENT_PROPERTY_REFERECENCECOMPONENT)) {
+                    String reference = sourceNode.getProperty(COMPONENT_PROPERTY_REFERECENCECOMPONENT).getString();
+                    if (!StringUtils.isBlank(reference)) {
+                        final HstComponentConfiguration targetReference = targetSite.getComponentsConfiguration().getComponentConfiguration(reference);
+                        final HstComponentConfiguration sourceReference = sourceSite.getComponentsConfiguration().getComponentConfiguration(reference);
+                        if (targetReference != null) {
+                            log.debug("Skipping '{}' and descendants because targetSite '{}' already has a resolvable reference for '{}'",
+                                    sourceConfig, targetSite, reference);
+                            if (sourceReference != null) {
+                                // sourceReference is never expected to be null, but just in case a null check
+                                populateSelfAndDescending(sourceReference, skipSet);
+                            }
+                            // no need to check descendant configs
+                            return;
+                        }
+                    }
+                }
+            } catch (RepositoryException e) {
+                throw new RuntimeRepositoryException(e);
+            }
+            for (HstComponentConfiguration child : sourceConfig.getChildren().values()) {
+                populateSkipSet(child, sourceSite, targetSite, session, skipSet);
+            }
+        }
+
+        private void populateSelfAndDescending(final HstComponentConfiguration current,
+                                               final Set<String> skipSet) {
+            skipSet.add(current.getCanonicalIdentifier());
+            for (HstComponentConfiguration child : current.getChildren().values()) {
+                populateSelfAndDescending(child, skipSet);
+            }
+        }
+
+        @Override
+        public boolean test(final HstComponentConfiguration sourceConfig) {
+            if (filteredConfigurationUUIDs.contains(sourceConfig.getCanonicalIdentifier())) {
+                return false;
+            }
+            return true;
+        }
     }
 
     private void translateFolders(final Session session, final Node sourceBaseFolderNode,
