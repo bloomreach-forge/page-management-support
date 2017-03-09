423b892dee462f02ebc22acf2c7a7d3449113510
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-03-09 11:24:54 +0100

    After moving to Github; copied the demo _as is_ from SVN trunk to a submodule

---
+++ demo/bootstrap/webfiles/src/main/resources/site/freemarker/channelpagesupdemo/onecolumn-main.ftl
@@ -0,0 +1,8 @@
+<#include "../include/imports.ftl">
+
+<div>
+  <h1>This is an instance of the one-column page prototype!</h1>
+
+
+  <@hst.include ref="container"/>
+</div>
---
+++ demo/site/src/main/java/org/onehippo/forge/channelmanager/pagesupport/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.channelmanager.pagesupport.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="channelpagesupdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
